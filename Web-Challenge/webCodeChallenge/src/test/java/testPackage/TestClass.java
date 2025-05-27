cac77fbf83f645c87745117a377a25356599e4f2
SondosNasserJoda <sondos.nasser@breadfast.com>
2025-05-27 14:09:39 +0300

    new

--- Web-Challenge/webCodeChallenge/src/test/java/testPackage/TestClass.java
+++ Web-Challenge/webCodeChallenge/src/test/java/testPackage/TestClass.java
@@ -3,20 +3,21 @@ package testPackage;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
-public class TestClass extends BaseTests{
+public class TestClass extends BaseTests {
     @Test
-    public void test(){
+    public void test() {
         searchBox.SearchBox("Laptop");
-       boolean displayingProduct = searchBox.WaitForElement();
+        boolean displayingProduct = searchBox.WaitForElement();
         assertTrue(displayingProduct);
     }
+
     @Test
-    public void SondosFirstTest(){
+    public void SondosFirstTest() {
 
     }
 
     @Test
-    public void SondosSecondTest(){
+    public void SondosSecondTest() {
 
     }
 
@@ -25,3 +26,4 @@ public class TestClass extends BaseTests{
 
 
     }
+}
